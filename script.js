ccaf318d4cdc1a7794d2f709beec9b80430bc022
abukato <artur.bukato@gmail.com>
2020-03-15 01:04:32 +0300

    refactor: singolo-dom tasks

--- script.js
+++ script.js
@@ -1,146 +1,219 @@
-const NAV = document.querySelector('.navigation');
+window.onload = () => {
 
-const V_PHONE = document.querySelector('.vertical-phone');
-const H_PHONE = document.querySelector('.horizontal-phone');
+  // Header
+  // TASK 1. Header Tabs
+  addNavigationClickHandler();
 
-const CAROUSEL = document.querySelector('.carousel');
-const SLIDES = document.querySelector('.carousel__slides');
-const PREV_BTN = document.querySelector('.button-prev');
-const NEXT_BTN = document.querySelector('.button-next');
+  // Slider
+  // TASK 2. Carousel functionality
+  // TASK 3. Turn Off Phone Displays of First Slide
+  addCarouselClickHandler();
 
-const TABS = document.querySelector('.portfolio-controls');
-const PORTFOLIO_LIST = document.querySelector('.portfolio__list');
+  // Portfolio
+  // TASK 4. TAGS switching & shuffle items
+  addTagsClickHandler();
+  // TASK 5. Selecting Images
+  addImagesClickHandler();
 
-const FORM = document.getElementById('feedback-form');
-const SUBMIT_BTN = document.getElementById('submit-btn');
-const CLOSE_BTN = document.getElementById('close-btn');
+  // Form
+  // TASK 6. Submitting Form
+  addSubmitButtonClickHandler();
+  addCloseButtonClickHandler();
+}
 
+// Header
+// TASK 1. Header Tabs
+const addNavigationClickHandler = () => {
+  let nav = document.querySelector('.navigation');
 
-// Header tabs
+  nav.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      let selectedLink = event.target;
+      selectClickedLink(selectedLink);
+    }
+  });
+}
 
-NAV.addEventListener('click', (event) => {
-  if (event.target.tagName === 'A') {
-    NAV.querySelectorAll('li a').forEach(elem => {
-      elem.classList.remove('navigation__link_active');
-      event.target.classList.add('navigation__link_active');
-    });
-  }
-});
+const selectClickedLink = (selectedLink) => {
+  let navigationLinks = document.querySelectorAll('.navigation .navigation__link');
 
+  navigationLinks.forEach(elem => {
+    elem.classList.remove('navigation__link_active');
+  });
+
+  selectedLink.classList.add('navigation__link_active');
+}
 
 // Slider
-// Display OFF
+// TASK 2. Carousel functionality
+// TASK 3. Turn Off Phone Displays of First Slide
+const CAROUSEL = document.querySelector('.carousel');
+const V_PHONE_DISPLAY = CAROUSEL.querySelector('.vertical-phone__display');
+const H_PHONE_DISPLAY = CAROUSEL.querySelector('.horizontal-phone__display');
 
-const phoneDisplayOff = (phone) => {
-  if (phone.querySelector('div').classList.contains('display-off')) {
-    phone.querySelector('div').classList.remove('display-off');
+const SLIDES_LIST = CAROUSEL.querySelector('.carousel__slides');
+
+const addCarouselClickHandler = () => {
+  let direction;
+
+  CAROUSEL.addEventListener('click', (event) => {
+    let clickedElement = event.target;
+    if (clickedElement.classList.contains('horizontal-phone__button')) {
+      turnOffDisplay(H_PHONE_DISPLAY);
+    } else if (clickedElement.classList.contains('vertical-phone__button')) {
+      turnOffDisplay(V_PHONE_DISPLAY);
+    } else if (clickedElement.classList.contains('button-prev')) {
+      previousSlide(direction);
+    } else if (clickedElement.classList.contains('button-next')) {
+      nextSlide(direction);
+    }
+  })
+
+  SLIDES_LIST.addEventListener('transitionend', function() {
+    slideSwap(direction);
+    setTimeout(() => {
+      SLIDES_LIST.style.transition = 'all 0.5s';
+    })
+  }, false);
+}
+
+const turnOffDisplay = (phoneDisplay) => {
+  if (phoneDisplay.classList.contains('display-off')) {
+    phoneDisplay.classList.remove('display-off');
   } else {
-    phone.querySelector('div').classList.add('display-off');
+    phoneDisplay.classList.add('display-off');
   }
 }
 
-V_PHONE.addEventListener('click', () => {
-  phoneDisplayOff(V_PHONE);
-})
-
-H_PHONE.addEventListener('click', () => {
-  phoneDisplayOff(H_PHONE);
-})
-
-// CAROUSEL
-
-let direction;
-
-NEXT_BTN.addEventListener('click', function() {
-  direction = -1;
-  CAROUSEL.style.justifyContent = 'flex-start';
-  SLIDES.style.transform = 'translate(-50%)';
-});
-
-PREV_BTN.addEventListener('click', function() {
+const previousSlide = (direction) => {
   if (direction === -1) {
     direction = 1;
-    SLIDES.appendChild(SLIDES.firstElementChild);
+    SLIDES_LIST.appendChild(SLIDES_LIST.firstElementChild);
   }
   CAROUSEL.style.justifyContent = 'flex-end';
-  SLIDES.style.transform = 'translate(50%)';
-});
+  SLIDES_LIST.style.transform = 'translate(50%)';
+}
 
-SLIDES.addEventListener('transitionend', function() {
+const nextSlide = (direction) => {
+  direction = -1;
+  CAROUSEL.style.justifyContent = 'flex-start';
+  SLIDES_LIST.style.transform = 'translate(-50%)';
+}
+
+const slideSwap = (direction) => {
   if (direction === 1) {
-    SLIDES.prepend(SLIDES.lastElementChild);
+    SLIDES_LIST.prepend(SLIDES_LIST.lastElementChild);
   } else {
-    SLIDES.appendChild(SLIDES.firstElementChild);
+    SLIDES_LIST.appendChild(SLIDES_LIST.firstElementChild);
   }
-  SLIDES.style.transition = 'none';
-  SLIDES.style.transform = 'translate(0)';
-  setTimeout(() => {
-    SLIDES.style.transition = 'all 0.7s';
-  })
-}, false);
+  SLIDES_LIST.style.transition = 'none';
+  SLIDES_LIST.style.transform = 'translate(0)';
+}
 
+// Portfolio
+// TASK 4. TAGS switching & shuffle items
+const addTagsClickHandler = () => {
+  document.querySelector('.portfolio-controls').addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+      let selectedTag = event.target;
+      switchingTags(selectedTag);
+      shuffleImages();
+    }
+  });
+}
 
-// Portfolio TABS && shuffle preview image && BORDERED image
+const switchingTags = (selectedTag) => {
+  document.querySelectorAll('.portfolio-controls button').forEach(button => {
+    button.classList.remove('portfolio-controls__button_active');
+    button.disabled = false;
+  });
+  selectedTag.classList.add('portfolio-controls__button_active');
+  selectedTag.disabled = true;
+}
 
-TABS.addEventListener('click', (event) => {
-  if (event.target.tagName === 'BUTTON') {
-    TABS.querySelectorAll('button').forEach(button => {
-      button.classList.remove('portfolio-controls__button_active');
-      button.disabled = false;
-      event.target.classList.add('portfolio-controls__button_active');
-      event.target.disabled = true;
-    });
-    PORTFOLIO_LIST.querySelectorAll('li').forEach(li => {
-      li.style.order = '';
-      li.style.order = `${Math.floor(Math.random()*100)}`;
-    });
-  }
-});
+const shuffleImages = () => {
+  document.querySelectorAll('.portfolio__preview').forEach(li => {
+    li.style.order = '';
+    li.style.order = `${Math.floor(Math.random()*100)}`;
+  });
+}
 
-PORTFOLIO_LIST.addEventListener('click', (event) => {
-  if (event.target.tagName === 'DIV') {
-    PORTFOLIO_LIST.querySelectorAll('div').forEach(div => {
-      div.parentNode.classList.remove('portfolio__preview_bordered');
-    });
-    event.target.parentNode.classList.add('portfolio__preview_bordered');
-  } else {
-    PORTFOLIO_LIST.querySelectorAll('div').forEach(div => {
-      div.parentNode.classList.remove('portfolio__preview_bordered');
-    });
-  }
-});
+// TASK 5. Selecting Images
 
+const addImagesClickHandler = () => {
+  document.querySelector('.portfolio__list').addEventListener('click', (event) => {
+    let selectedImage = event.target;
+    if (selectedImage.classList.contains('preview')) {
+      borderedSelectingImage(selectedImage);
+    } else {
+      removeBorder();
+    }
+  });
+}
 
-// FORM SUBMIT
+const borderedSelectingImage = (selectedImage) => {
+  document.querySelectorAll('.portfolio__list .preview').forEach(div => {
+    div.parentNode.classList.remove('portfolio__preview_bordered');
+  });
+  selectedImage.parentNode.classList.add('portfolio__preview_bordered');
+}
 
+const removeBorder = () => {
+  document.querySelectorAll('.portfolio__list .preview').forEach(div => {
+    div.parentNode.classList.remove('portfolio__preview_bordered');
+  });
+}
 
-SUBMIT_BTN.addEventListener('click', (event) => {
-  const POPUP_MESSAGE = document.getElementById('popup__message');
+// Form
+// TASK 6. Submitting Form
+
+const addSubmitButtonClickHandler = () => {
+  document.querySelector('.submit-btn').addEventListener('click', (event) => {
+    if (document.querySelector('.feedback-form').checkValidity()) {
+      event.preventDefault();
+      createModalMessage();
+      showModalWithResult(MODAL_MESSAGE);
+    }
+  });
+};
+
+const createModalMessage = () => {
+  const regExp = /<.*?>/g;
 
   const SUBJECT_VALUE = document.getElementById('subject').value 
-    ? `Тема: ${document.getElementById('subject').value}` 
+    ? `Тема: ${document.getElementById('subject').value.replace(regExp)}` 
     : `Без темы`;
+
   const MESSAGE_VALUE = document.getElementById('message').value 
-    ? `Описание: ${document.getElementById('message').value}` 
+    ? `Описание: ${document.getElementById('message').value.replace(regExp)}` 
     : `Без описания`;
 
-  if (FORM.checkValidity()) {
-    document.querySelector('.popup').classList.remove('hidden');
+  MODAL_MESSAGE = `<h4>Письмо отправлено</h4><p>${SUBJECT_VALUE}</p><p>${MESSAGE_VALUE}</p>`;
+  return MODAL_MESSAGE;
+}
 
-    POPUP_MESSAGE.insertAdjacentHTML('afterbegin', 
-      `<h4>Письмо отправлено</h4>
-      <p>${SUBJECT_VALUE}</p>
-      <p>${MESSAGE_VALUE}</p>`
-    );
-    event.preventDefault();
-  }
-});
+const showModalWithResult = (MODAL_MESSAGE) => {
+  document.querySelector('.modal').classList.remove('hidden');
+  document.querySelector('.modal__message').insertAdjacentHTML('afterbegin', MODAL_MESSAGE);
+  document.body.style.overflow = 'hidden';
+}
 
-CLOSE_BTN.addEventListener('click', (event) => {
-  document.querySelector('.popup').classList.add('hidden');
-  document.querySelector('.popup__message h4').remove();
-  document.querySelectorAll('.popup__message p').forEach(p => {
+const addCloseButtonClickHandler = () => {
+  document.querySelector('.close-btn').addEventListener('click', () => {
+    closeModalWindow();
+    clearModalWindow();
+    document.querySelector('.feedback-form').reset();
+  });
+}
+
+const closeModalWindow = () => {
+  document.querySelector('.modal').classList.add('hidden');
+  document.body.style.overflow = '';
+}
+
+const clearModalWindow = () => {
+  document.querySelector('.modal__message h4').remove();
+  document.querySelectorAll('.modal__message p').forEach(p => {
     p.remove();
   });
-  FORM.reset();
-});
+}
